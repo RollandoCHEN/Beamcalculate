55e4e14817c13dfd683718dc7869d10d4c91241a
Ruolin <m.ruolinchen@gmail.com>
2017-09-24 22:58:47 +0200

    Back to a enum class for the calculate methods, and make the language change work for the methods name

---
+++ src/main/java/com/beamcalculate/enums/CalculateMethod.java
@@ -0,0 +1,21 @@
+package com.beamcalculate.enums;
+
+import com.beamcalculate.Main;
+
+public enum CalculateMethod {
+    CAQUOT("enum.method.caquot"),
+    CAQUOT_MINOREE("enum.method.caquot.reduced"),
+    FORFAITAIRE("enum.method.forfaitaire"),
+    TROIS_MOMENT("enum.method.threeMoment"),
+    TROIS_MOMENT_R("enum.method.threeMoment.redistribution");
+
+    private String mBundleTextKey;
+
+    CalculateMethod(String bundleTextKey) {
+        mBundleTextKey = bundleTextKey;
+    }
+
+    public String getBundleTextKey() {
+        return Main.getBundleText(mBundleTextKey);
+    }
+}
