a96726051b7544d663f106f73f8a55254341e644
Ruolin <m.ruolinchen@gmail.com>
2017-09-24 22:52:20 +0200

    Merge the method name enum to the support moment class

--- src/main/java/com/beamcalculate/model/calculate/support/SupportMoment.java
+++ src/main/java/com/beamcalculate/model/calculate/support/SupportMoment.java
@@ -7,20 +7,20 @@ import java.util.Map;
 
 public abstract class SupportMoment {
     public enum MethodName{
-        CAQUOT(Main.getBundleText("enum.method.caquot")),
-        CAQUOT_MINOREE(Main.getBundleText("enum.method.caquot.reduced")),
-        FORFAITAIRE(Main.getBundleText("enum.method.forfaitaire")),
-        TROIS_MOMENT(Main.getBundleText("enum.method.threeMoment")),
-        TROIS_MOMENT_R(Main.getBundleText("enum.method.threeMoment.redistribution"));
+        CAQUOT("enum.method.caquot"),
+        CAQUOT_MINOREE("enum.method.caquot.reduced"),
+        FORFAITAIRE("enum.method.forfaitaire"),
+        TROIS_MOMENT("enum.method.threeMoment"),
+        TROIS_MOMENT_R("enum.method.threeMoment.redistribution");
 
-        private String mMethodName;
+        private String mBundleTextKey;
 
-        MethodName(String methodName) {
-            mMethodName = methodName;
+        MethodName(String bundleTextKey) {
+            mBundleTextKey = bundleTextKey;
         }
 
-        public String getMethodName() {
-            return mMethodName;
+        public String getBundleTextKey() {
+            return Main.getBundleText(mBundleTextKey);
         }
     }
 
